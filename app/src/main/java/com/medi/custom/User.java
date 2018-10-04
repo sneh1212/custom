960b5f59df8aa70dbcb1073f9ec8979f2ca118eb
cselab <kumarsnehshahu@gmail.com>
2018-10-04 15:12:26 +0530

    Initial commit

---
+++ app/src/main/java/com/medi/custom/User.java
@@ -0,0 +1,16 @@
+package com.medi.custom;
+
+public class User {
+    public String name, email, phone,gender;
+
+    public User(){
+
+    }
+
+    public User(String name, String email, String phone,String gender) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+    }
+}
